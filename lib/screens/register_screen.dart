093d703d507a935673c25c3022b8d2909d0dc171
Chette <80092129+MrChette@users.noreply.github.com>
2022-11-18 09:49:17 +0100

    Register validator message :alien:

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -35,37 +35,37 @@ class RegisterScreen extends StatelessWidget {
             child: SingleChildScrollView(
       child: Column(
         children: [
-          SizedBox(height: 250),
+          const SizedBox(height: 250),
           CardContainer(
               child: Column(
             children: [
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               Text('Crear cuenta',
                   style: Theme.of(context).textTheme.headline4),
-              SizedBox(height: 30),
+              const SizedBox(height: 30),
               ChangeNotifierProvider(
                   create: (_) => RegisterFormProvider(), child: _RegisterForm())
             ],
           )),
-          SizedBox(height: 50),
+          const SizedBox(height: 50),
           TextButton(
               onPressed: () => Navigator.pushReplacementNamed(context, 'login'),
               style: ButtonStyle(
                   overlayColor:
                       MaterialStateProperty.all(Colors.indigo.withOpacity(0.1)),
-                  shape: MaterialStateProperty.all(StadiumBorder())),
-              child: Text(
+                  shape: MaterialStateProperty.all(const StadiumBorder())),
+              child: const Text(
                 '¿Ya tienes una cuenta?',
                 style: TextStyle(fontSize: 18, color: Colors.black87),
               )),
-          SizedBox(height: 50),
+          const SizedBox(height: 50),
         ],
       ),
     )));
   }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatelessWidget with InputValidationMixin {
   @override
   Widget build(BuildContext context) {
     final ciclesProvider = Provider.of<GetCicles>(context);
@@ -78,150 +78,149 @@ class _RegisterForm extends StatelessWidget {
 
     final registerForm = Provider.of<RegisterFormProvider>(context);
 
-    return Container(
-      child: Form(
-        key: registerForm.formKey,
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        child: Column(
-          // ignore: sort_child_properties_last
-          children: [
-            TextFormField(
-              autocorrect: false,
-              keyboardType: TextInputType.name,
-              decoration: InputDecorations.authInputDecoration(
-                  hintText: '', labelText: 'Name', prefixIcon: Icons.person),
-              onChanged: (value) => registerForm.name = value,
-              validator: (value) {
-                return (value != null && value.length > 1)
-                    ? null
-                    : 'Name field cant be null';
-              },
-            ),
-            TextFormField(
-              autocorrect: false,
-              keyboardType: TextInputType.name,
-              decoration: InputDecorations.authInputDecoration(
-                  hintText: '', labelText: 'Surname', prefixIcon: Icons.person),
-              onChanged: (value) => registerForm.surname = value,
-              validator: (value) {
-                return (value != null && value.length > 1)
-                    ? null
-                    : 'Surname field cant be null';
-              },
-            ),
-            TextFormField(
-              autocorrect: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecorations.authInputDecoration(
-                  hintText: 'john.doe@gmail.com',
-                  labelText: 'Correo electrónico',
-                  prefixIcon: Icons.alternate_email_rounded),
-              onChanged: (value) => registerForm.email = value,
-              validator: (value) {
-                String pattern =
-                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-                RegExp regExp = RegExp(pattern);
+    return Form(
+      key: registerForm.formKey,
+      // autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: Column(
+        // ignore: sort_child_properties_last
+        children: [
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.name,
+            decoration: InputDecorations.authInputDecoration(
+                hintText: '', labelText: 'Name', prefixIcon: Icons.person),
+            onChanged: (value) => registerForm.name = value,
+            validator: (name) {
+              if (isTextValid(name)) {
+                return null;
+              } else {
+                return 'Name field cant be null';
+              }
+            },
+          ),
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.name,
+            decoration: InputDecorations.authInputDecoration(
+                hintText: '', labelText: 'Surname', prefixIcon: Icons.person),
+            onChanged: (value) => registerForm.surname = value,
+            validator: (surname) {
+              if (isTextValid(surname)) {
+                return null;
+              } else {
+                return 'Surname field cant be null';
+              }
+            },
+          ),
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecorations.authInputDecoration(
+                hintText: 'john.doe@gmail.com',
+                labelText: 'Correo electrónico',
+                prefixIcon: Icons.alternate_email_rounded),
+            onChanged: (value) => registerForm.email = value,
+            validator: (email) {
+              if (isEmailValid(email)) {
+                return null;
+              } else {
+                return 'Enter a valid email address';
+              }
+            },
+          ),
+          TextFormField(
+            autocorrect: false,
+            obscureText: true,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecorations.authInputDecoration(
+                hintText: '*******',
+                labelText: 'Contraseña',
+                prefixIcon: Icons.lock_outline),
+            onChanged: (value) => registerForm.password = value,
+            validator: (password) {
+              if (isPasswordValid(password)) {
+                return null;
+              } else {
+                return 'Enter a valid password';
+              }
+            },
+          ),
+          TextFormField(
+            autocorrect: false,
+            obscureText: true,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecorations.authInputDecoration(
+                hintText: '*******',
+                labelText: 'Password',
+                prefixIcon: Icons.lock_outline),
+            validator: (value) {
+              return (value == registerForm.password)
+                  ? null
+                  : "The passwords don't match";
+            },
+          ),
+          DropdownButtonFormField<Ciclos>(
+            hint: const Text('Select cicle'),
+            // value: selectedItem,
+            items: options
+                .map(
+                  (courseName) => DropdownMenuItem(
+                    value: courseName,
+                    child: Text(courseName.nameCicle),
+                  ),
+                )
+                .toList(),
+            onChanged: (value) {
+              registerForm.cicle_id = (value?.idCicle.toInt())!;
+            },
+          ),
+          const SizedBox(height: 30),
+          MaterialButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              onPressed: registerForm.isLoading
+                  ? null
+                  : () async {
+                      FocusScope.of(context).unfocus();
+                      final authService =
+                          Provider.of<AuthService>(context, listen: false);
 
-                return regExp.hasMatch(value ?? '')
-                    ? null
-                    : 'El valor ingresado no luce como un correo';
-              },
-            ),
-            TextFormField(
-              autocorrect: false,
-              obscureText: true,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecorations.authInputDecoration(
-                  hintText: '*******',
-                  labelText: 'Contraseña',
-                  prefixIcon: Icons.lock_outline),
-              onChanged: (value) => registerForm.password = value,
-              validator: (value) {
-                return (value != null && value.length >= 8)
-                    ? null
-                    : 'The password length must be longer than 8';
-              },
-            ),
-            TextFormField(
-              autocorrect: false,
-              obscureText: true,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecorations.authInputDecoration(
-                  hintText: '*******',
-                  labelText: 'Password',
-                  prefixIcon: Icons.lock_outline),
-              onChanged: (value) => registerForm.c_password = value,
-              validator: (value) {
-                return (value == registerForm.password)
-                    ? null
-                    : "The passwords don't match";
-              },
-            ),
-            DropdownButtonFormField<Ciclos>(
-              hint: Text('Select cicle'),
-              // value: selectedItem,
-              items: options
-                  .map(
-                    (courseName) => DropdownMenuItem(
-                      value: courseName,
-                      child: Text(courseName.nameCicle),
-                    ),
-                  )
-                  .toList(),
-              onChanged: (value) {
-                registerForm.cicle_id = (value?.idCicle.toInt())!;
-              },
-            ),
-            const SizedBox(height: 30),
-            MaterialButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                disabledColor: Colors.grey,
-                elevation: 0,
-                color: Colors.deepPurple,
-                onPressed: registerForm.isLoading
-                    ? null
-                    : () async {
-                        FocusScope.of(context).unfocus();
-                        final authService =
-                            Provider.of<AuthService>(context, listen: false);
+                      if (!registerForm.isValidForm()) return;
 
-                        if (!registerForm.isValidForm()) return;
+                      // registerForm.isLoading = true;
 
-                        // registerForm.isLoading = true;
-
-                        //validar si el login es correcto
-                        final String? errorMessage =
-                            await authService.createUser(
-                                registerForm.name,
-                                registerForm.surname,
-                                registerForm.email,
-                                registerForm.password,
-                                registerForm.c_password,
-                                registerForm.cicle_id);
-                        // if (registerForm.password != registerForm.c_password) {
-                        //   customToast("The passwords don't match", context);
-                        //   registerForm.isLoading = false;
-                        // } else {
-                        if (errorMessage == null) {
-                          Navigator.pushReplacementNamed(context, 'home');
-                        } else {
-                          //mostrar error en pantalla
-                          customToast(
-                              'The email is already registered', context);
-                          registerForm.isLoading = false;
-                        }
-                        // }
-                      },
-                child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 80, vertical: 15),
-                    child: Text(
-                      registerForm.isLoading ? 'Espere' : 'Registrar',
-                      style: const TextStyle(color: Colors.white),
-                    )))
-          ],
-        ),
+                      //validar si el login es correcto
+                      final String? errorMessage = await authService.createUser(
+                          registerForm.name,
+                          registerForm.surname,
+                          registerForm.email,
+                          registerForm.password,
+                          registerForm.c_password,
+                          registerForm.cicle_id);
+                      // if (registerForm.password != registerForm.c_password) {
+                      //   customToast("The passwords don't match", context);
+                      //   registerForm.isLoading = false;
+                      // } else {
+                      if (errorMessage == null) {
+                        Navigator.pushReplacementNamed(context, 'home');
+                      } else {
+                        //mostrar error en pantalla
+                        customToast('The email is already registered', context);
+                        registerForm.isLoading = false;
+                      }
+                      // }
+                    },
+              child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                  child: Text(
+                    registerForm.isLoading ? 'Espere' : 'Registrar',
+                    style: const TextStyle(color: Colors.white),
+                  )))
+        ],
       ),
     );
   }
@@ -233,10 +232,23 @@ class _RegisterForm extends StatelessWidget {
       animation: StyledToastAnimation.scale,
       reverseAnimation: StyledToastAnimation.fade,
       position: StyledToastPosition.top,
-      animDuration: Duration(seconds: 1),
-      duration: Duration(seconds: 4),
+      animDuration: const Duration(seconds: 1),
+      duration: const Duration(seconds: 4),
       curve: Curves.elasticOut,
       reverseCurve: Curves.linear,
     );
+  }
+}
+
+mixin InputValidationMixin {
+  bool isTextValid(texto) => texto.length > 0;
+
+  bool isPasswordValid(password) => password.length == 6;
+
+  bool isEmailValid(email) {
+    String pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regExp = RegExp(pattern);
+    return regExp.hasMatch(email);
   }
 }
