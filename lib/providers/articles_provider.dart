7c7daf767a955b09a3c1c4e83532e3c100dd8ce3
Chette <80092129+MrChette@users.noreply.github.com>
2022-12-19 19:58:47 +0100

    Listando articles softDeleted

--- lib/providers/articles_provider.dart
+++ lib/providers/articles_provider.dart
@@ -6,7 +6,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 import '../Models/articles.dart';
-import 'auth_service.dart';
+import '../services/auth_service.dart';
 
 class ArticleService extends ChangeNotifier {
   final String _baseUrl = 'semillero.allsites.es';
@@ -14,24 +14,24 @@ class ArticleService extends ChangeNotifier {
   final List<ArticleData> articles = [];
   bool isLoading = true;
 
-  getArticles() async {
-    articles.clear();
+  ArticleService();
 
+  getArticles() async {
     String? token = await AuthService().readToken();
     isLoading = true;
     notifyListeners();
     final url = Uri.http(_baseUrl, '/public/api/articles');
-    final resp = await http.get(url, headers: {
+    final resp = await http.post(url, headers: {
       'Content-type': 'application/json',
       'Accept': 'application/json',
       'Authorization': 'Bearer $token',
     });
     final Map<String, dynamic> decodedResp = json.decode(resp.body);
-    // print(decodedResp);
     var p = Articles.fromJson(decodedResp);
     for (var i in p.data!) {
-      articles.add(i);
-      // print(i.deleted);
+      if (i.deleted == 0) {
+        articles.add(i);
+      }
     }
     isLoading = false;
     notifyListeners();
--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -1,4 +1,5 @@
 import 'package:almagest/Models/models.dart';
+import 'package:almagest/screens/catalog_screen.dart';
 import 'package:almagest/services/services.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -13,8 +14,7 @@ class UserScreen extends StatefulWidget {
 class _UserScreenState extends State<UserScreen> {
   final articleService = ArticleService();
   List<ArticleData> articles = [];
-
-  Future getArticle() async {
+  Future getArticles() async {
     await articleService.getArticles();
     setState(() {
       articles = articleService.articles;
@@ -22,42 +22,32 @@ class _UserScreenState extends State<UserScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    getArticles();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final authService = Provider.of<AuthService>(context, listen: false);
+    // final articleService = Provider.of<ArticleService>(context, listen: false);
+    // articles = articleService.articles.cast<ArticleData>();
+    for (int i = 0; i < articles.length; i++) {
+      if (articles[i].deleted == 1) {
+        print(articles[i].name);
+      }
+    }
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('User'),
-          centerTitle: true,
-          leading: IconButton(
-            icon: const Icon(Icons.login_outlined),
-            onPressed: () {
-              authService.logout();
-              Navigator.pushReplacementNamed(context, 'login');
-            },
-          ),
+      appBar: AppBar(
+        title: const Text('Articles'),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.login_outlined),
+          onPressed: () {
+            Provider.of<AuthService>(context, listen: false).logout();
+            Navigator.pushReplacementNamed(context, 'login');
+          },
         ),
-        body: articleService.isLoading
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : SingleChildScrollView(
-                child: Center(
-                child: Column(children: [
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Visibility(
-                    visible: true,
-                    child: SizedBox(
-                      height: 80,
-                      width: MediaQuery.of(context).size.width / 1.5,
-                      child: Center(
-                        child: Text('Catalog',
-                            style: Theme.of(context).textTheme.headline2),
-                      ),
-                    ),
-                  ),
-                ]),
-              )));
+      ),
+    );
   }
 }
