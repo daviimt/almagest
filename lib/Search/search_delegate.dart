83c4f0e001f9ffa8bdaf125af73ff9db8ca44b56
daviimt <dmateomerino@gmail.com>
2022-12-20 09:14:49 +0100

    search delegate recive data

--- lib/Search/search_delegate.dart
+++ lib/Search/search_delegate.dart
@@ -1,4 +1,7 @@
+import 'package:almagest/Models/articles.dart';
 import 'package:flutter/material.dart';
+
+import '../services/services.dart';
 
 class MovieSearchDelegate extends SearchDelegate {
   @override
@@ -21,19 +24,37 @@ class MovieSearchDelegate extends SearchDelegate {
     return Text('data');
   }
 
+  Widget _emptyContainer() {
+    return Container(
+      child: Center(
+        child: Icon(
+          Icons.movie_creation_outlined,
+          color: Colors.black,
+          size: 130,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget buildSuggestions(BuildContext context) {
     if (query.isEmpty) {
-      return Container(
-        child: Center(
-          child: Icon(
-            Icons.movie_creation_outlined,
-            color: Colors.black,
-            size: 130,
-          ),
-        ),
-      );
+      return _emptyContainer();
     } else {}
-    return Container();
+
+    final articleService = ArticleService();
+    List<ArticleData> articles = [];
+    Future getArticles() async {
+      await articleService.getArticles();
+    }
+
+    return FutureBuilder(
+      future: articleService.getArticles(),
+      builder: (_, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData) return _emptyContainer();
+
+        return Container();
+      },
+    );
   }
 }
