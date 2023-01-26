b2ce4055eef9a456d14e2068d172a7833ffe3f2d
daviimt <dmateomerino@gmail.com>
2023-01-26 10:27:28 +0100

    formOrder conn lista despegable de compañias

--- lib/screens/form_order_screen.dart
+++ lib/screens/form_order_screen.dart
@@ -7,6 +7,8 @@ import 'package:flutter_styled_toast/flutter_styled_toast.dart';
 import 'package:getwidget/getwidget.dart';
 import 'package:provider/provider.dart';
 
+import '../ui/input_decorations.dart';
+
 class FormOrderScreen extends StatelessWidget {
   const FormOrderScreen({Key? key}) : super(key: key);
 
@@ -14,6 +16,15 @@ class FormOrderScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final ordersService = Provider.of<OrdersService>(context);
     List<OrdersData> orders = ordersService.orders;
+    final ciclesProvider = Provider.of<GetCompanies>(context);
+    List<Companies> ciclos = ciclesProvider.getAllCompanies;
+    List<Companies> options = [];
+    if (ciclos.isNotEmpty) {
+      for (var i = 0; i < ciclos.length; i++) {
+        options.add(ciclos[i]);
+      }
+    }
+
     // ignore: no_leading_underscores_for_local_identifiers
 
     return Scaffold(
@@ -30,88 +41,22 @@ class FormOrderScreen extends StatelessWidget {
           ? const Center(
               child: CircularProgressIndicator(),
             )
-          : SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Center(
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 20,
+          : DropdownButtonFormField<Companies>(
+              decoration: InputDecorations.authInputDecoration(
+                  prefixIcon: Icons.view_week_outlined,
+                  hintText: '',
+                  labelText: 'Company'),
+              // value: selectedItem,
+              items: options
+                  .map(
+                    (courseName) => DropdownMenuItem(
+                      value: courseName,
+                      child: Text(courseName.nameCompanie),
                     ),
-                    ListView.separated(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      padding: const EdgeInsets.all(30),
-                      itemCount: orders.length,
-                      itemBuilder: (BuildContext context, index) {
-                        return SizedBox(
-                          height: 250,
-                          child: Card(
-                            elevation: 20,
-                            child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceEvenly,
-                                      children: [
-                                        Text('${orders[index].num}',
-                                            style:
-                                                const TextStyle(fontSize: 20)),
-                                        Text('${orders[index].issueDate}',
-                                            style:
-                                                const TextStyle(fontSize: 20))
-                                      ]),
-                                  const Divider(color: Colors.black),
-                                  Text('${orders[index].targetCompanyName}',
-                                      style: const TextStyle(fontSize: 35),
-                                      textAlign: TextAlign.center),
-                                  Text('${orders[index].createdAt}',
-                                      style: const TextStyle(fontSize: 35),
-                                      textAlign: TextAlign.center),
-                                  const Divider(color: Colors.black),
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      const Divider(color: Colors.black),
-                                      if (orders[index].deliveryNotes == 1)
-                                        Icon(
-                                          Icons.delivery_dining,
-                                          color: Colors.greenAccent,
-                                        )
-                                      else
-                                        Icon(
-                                          Icons.delivery_dining,
-                                          color: Colors.red,
-                                        ),
-                                      if (orders[index].invoices == 1)
-                                        Icon(
-                                          Icons.library_books_outlined,
-                                          color: Colors.greenAccent,
-                                        )
-                                      else
-                                        Icon(
-                                          Icons.library_books_outlined,
-                                          color: Colors.red,
-                                        )
-                                    ],
-                                  ),
-                                ]),
-                          ),
-                        );
-                      },
-                      separatorBuilder: (BuildContext context, int index) {
-                        return const Divider();
-                      },
-                    ),
-                    SizedBox(
-                      child: Container(),
-                    ),
-                  ],
-                ),
-              ),
+                  )
+                  .toList(),
+              onChanged: (value) {},
+              validator: (cicle) {},
             ),
     );
   }
